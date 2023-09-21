d69223631c8d83a7ead9c0a8121ae6e5cf61b12d
joshxgchen <josh_chen@sfu.ca>
2023-09-20 21:39:13 -0700

    fix

--- test/test_page.py
+++ test/test_page.py
@@ -20,7 +20,7 @@ def test_nonsecret_scenario():
     #with wait_for_page_load(driver): removed bitches
     #    pass
 
-    response_text = driver.find_element(By.XPATH, "/html/body")
+    response_text = driver.find_element(By.XPATH, "/html/body").text
     #assert "Thank you, Bob, for supporting Simple Web Page Industries. We appreciate your business." in response_text
     assert "Thank you, Josh" in response_text
     assert "We also like Sushi" in response_text
@@ -49,7 +49,7 @@ def test_secret_scenario():
     #with wait_for_page_load(driver): removed bitches
     #    pass
 
-    response_text = driver.find_element(By.XPATH, "/html/body")
+    response_text = driver.find_element(By.XPATH, "/html/body").text
     #assert "Thank you, Bob, for supporting Simple Web Page Industries. We appreciate your business." in response_text
     # Assuming this element is present in response.html with the ID "response_text"
     assert "Thank you, Josh" in response_text
