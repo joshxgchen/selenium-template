ba6afd8e68d6b7de475fae2f555aa2ea64374180
joshxgchen <josh_chen@sfu.ca>
2023-09-20 21:17:08 -0700

    update

--- test/test_page.py
+++ test/test_page.py
@@ -1,77 +1,91 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from e1utils import construct_headless_chrome_driver, get_landing_page_url, wait_for_page_load
 
+
 def test_nonsecret_scenario():
+    landing_page = get_landing_page_url()
     driver = construct_headless_chrome_driver()
-    driver.get(get_landing_page_url())
 
-    name_input = driver.find_element(By.ID, "preferredname")
-    food_input = driver.find_element(By.ID, "food")
-    password_input = driver.find_element(By.ID, "secret")  # Corrected the element ID
+    try:
+        # Open the landing page
+        driver.get(landing_page)
 
-    name_input.send_keys("Josh")
-    food_input.send_keys("Sushi")
-    password_input.send_keys("password123")
+        # Find and interact with form elements
+        name_input = driver.find_element(By.ID, "preferredname")
+        food_input = driver.find_element(By.ID, "food")
+        secret_input = driver.find_element(By.ID, "secret")
+        submit_button = driver.find_element(By.ID, "submit")
 
-    submit_button = driver.find_element(By.ID, "submit")
-    submit_button.click()
+        # Fill in form fields
+        name_input.send_keys("John Doe")
+        food_input.send_keys("Pizza")
+        secret_input.send_keys("mysecretcode")
 
-    #with wait_for_page_load(driver): removed bitches
-    #    pass
+        # Submit the form
+        submit_button.click()
 
-    assert "response.html" in driver.current_url
+        # Wait for the response page to load
+        WebDriverWait(driver, 10).until(EC.url_to_be("http://yourwebsite.com/response.html"))
 
-    # Assuming this element is present in response.html with the ID "response_text"
-    response_text = driver.find_element(By.ID, "response_text").text
-    assert "Thank you, Josh" in response_text
-    assert "We also like Sushi" in response_text
+        # Assert expected content on the response page
+        assert "Thank you, John Doe," in driver.page_source
+        assert "We also really like Pizza," in driver.page_source
 
-    # Assuming the button is not present in response.html
-    secret_button = driver.find_elements(By.ID, "secret_button")
-    assert len(secret_button) == 0
+        # Assert that the button is not present on the page
+        assert len(driver.find_elements(By.ID, "secretButton")) == 0
 
-    driver.quit()
+    finally:
+        driver.quit()
+
 
 def test_secret_scenario():
+    landing_page = get_landing_page_url()
     driver = construct_headless_chrome_driver()
-    driver.get(get_landing_page_url())
 
-    name_input = driver.find_element(By.ID, "preferredname")
-    food_input = driver.find_element(By.ID, "food")
-    password_input = driver.find_element(By.ID, "secret")  # Corrected the element ID
+    try:
+        # Open the landing page
+        driver.get(landing_page)
 
-    name_input.send_keys("Josh")
-    food_input.send_keys("Sushi")
-    password_input.send_keys("magic")  # Updated to use the correct secret code
+        # Find and interact with form elements
+        name_input = driver.find_element(By.ID, "preferredname")
+        food_input = driver.find_element(By.ID, "food")
+        secret_input = driver.find_element(By.ID, "secret")
+        submit_button = driver.find_element(By.ID, "submit")
 
-    submit_button = driver.find_element(By.ID, "submit")
-    submit_button.click()
+        # Fill in form fields
+        name_input.send_keys("Jane Smith")
+        food_input.send_keys("Sushi")
+        secret_input.send_keys("magic")
 
-    #with wait_for_page_load(driver): removed bitches
-    #    pass
+        # Submit the form
+        submit_button.click()
 
-    assert "response.html" in driver.current_url
+        # Wait for the response page to load
+        WebDriverWait(driver, 10).until(EC.url_to_be("http://yourwebsite.com/response.html"))
 
-    # Assuming this element is present in response.html with the ID "response_text"
-    response_text = driver.find_element(By.ID, "response_text").text
-    assert "Thank you, Josh" in response_text
-    assert "We also like Sushi" in response_text
+        # Assert expected content on the response page
+        assert "Thank you, Jane Smith," in driver.page_source
+        assert "We also really like Sushi," in driver.page_source
 
-    # Assuming the button is present in response.html
-    secret_button = driver.find_element(By.ID, "secret_button")
-    assert secret_button.is_displayed()
+        # Check for the "Take me to the secrets" button
+        assert "Take me to the secrets." in driver.page_source
 
-    secret_button.click()
+        # Click the "Take me to the secrets" button
+        secret_button = driver.find_element(By.ID, "secretButton")
+        secret_button.click()
 
-    with wait_for_page_load(driver):
-        pass
+        # Wait for the secret page to load
+        WebDriverWait(driver, 10).until(EC.url_to_be("http://yourwebsite.com/secret.html"))
 
-    assert "secret.html" in driver.current_url
+        # Assert expected content on the secret page
+        assert "Thanks for being an extra special supporter, Jane Smith." in driver.page_source
+        assert "We know that you find our company has a little extra magic." in driver.page_source
 
-    # Assuming this element is present in secret.html with the ID "secret_text"
-    secret_text = driver.find_element(By.ID, "secret_text").text
-    assert "Thank you, Josh" in secret_text
-    assert "You entered the secret code: magic" in secret_text
+    finally:
+        driver.quit()
 
-    driver.quit()
+
