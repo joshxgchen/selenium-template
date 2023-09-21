f58fc0616f71ed2b28ae516c37a5bfa8550618c0
joshxgchen <josh_chen@sfu.ca>
2023-09-20 21:52:33 -0700

    typo

--- test/e1utils.py
+++ test/e1utils.py
@@ -19,7 +19,7 @@ def construct_headless_chrome_driver():
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
 
     if os.path.exists(SNAP_LOCATION):
         path = SNAP_LOCATION
